4a517054b1bf27559894e9f9e18e8285621c2ae8
Nikolai Nagornykh <mikalai.nahornykh@innowise-group.com>
2022-11-25 14:28:25 +0300

    initialize app parts

---
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from '../services/local-storage.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private localStorage: LocalStorageService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.localStorage.getValue('accessToken');
+    request = request.clone({
+      setHeaders: {
+        Authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+    return next.handle(request);
+  }
+}
